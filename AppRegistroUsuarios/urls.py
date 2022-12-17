c90b52aab7c5219d38b80efc1a4ea9973fc8d5de
GuadalupeAlori <guadalupealori@yahoo.com.ar>
2022-12-17 10:28:38 -0300

    Herencia de templates. Cargar cs

--- AppRegistroUsuarios/urls.py
+++ AppRegistroUsuarios/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from AppRegistroUsuarios.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path ("", inicio, name = "inicio"),
     path ("registro/", registro_usuario, name = "registro_usuario"),
     path ("ingreso/", Pagina_de_ingreso, name = "Pagina_de_ingreso" ),
+    path("desconectarse/", desconectarse, name = "desconectarse" ),
+    path("prueba/", prueba),
 
 ]
--- AppRegistroUsuarios/views.py
+++ AppRegistroUsuarios/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from AppRegistroUsuarios.registroForm import CrearUsuario
 from django.contrib.auth.forms import AuthenticationForm
 
 def inicio(request):
     return render (request, "Inicio.html")
 
-#--- Pagina de registro---#
+#--- Pagina de registro ---#
 
 def registro_usuario(request):
     if request.method=="POST":
@@ -23,22 +23,31 @@ def registro_usuario(request):
 
 def Pagina_de_ingreso(request):
     if request.method == "POST":
-        form=AuthenticationForm(request.POST)
+        form=AuthenticationForm(request, data = request.POST)
         if form.is_valid():
-            nombreusuario = form.cleaned_data.get["username"]
-            claveusuario = form.cleaned_data.get["password"]
+            nombreusuario = form.cleaned_data.get("username")
+            claveusuario = form.cleaned_data.get("password")
             usuario = authenticate (username=nombreusuario, password=claveusuario)
             if usuario is not None:
-                Pagina_de_ingreso (request, usuario)
-                return render (request, "inicio.html")
+                login(request, usuario)
+                return render (request, "Inicio.html")
             else:
-                return render (request, "Pagina_de_ingreso.html")
+                return render (request, "Pagina_de_ingreso.html", {"form":form})
         else:
-            return render (request, "Pagina_de_ingreso.html")
+            return render (request, "Pagina_de_ingreso.html",{"form":form})
     else:
         form=AuthenticationForm()
     
-    return render (request, "Pagina_de_ingreso.html")
+    return render (request, "Pagina_de_ingreso.html",{"form":form})
+
+#--- Logout ---#
+
+def desconectarse(request):
+    logout(request)
+    return render(request, "Inicio.html", {"mensaje": "Desconectado existosamente"})
+
+def prueba(request):
+    return render(request, "index.html")
 
             
 
